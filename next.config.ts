c8e6e8343a76e1b537e681a1b85f0e4e6e5da9c2
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-03-10 20:40:19 +0700

    refactor: remove unused image configurations and API rewrites from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,35 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "storage.shngm.id",
-      },
-    ],
-  },
-
-  rewrites: async () => {
-    return [
-      // {
-      //   source: "/v1/api/mangas/:id/chapters/:chapter",
-      //   destination: "https://api.shngm.io/v1/chapter/detail/:chapter",
-      // },
-      // {
-      //   source: "/v1/api/mangas/:id/chapters/:path*",
-      //   destination: "https://api.shngm.io/v1/chapter/:id/list/:path*",
-      // },
-      // {
-      //   source: "/v1/api/mangas/:id",
-      //   destination: "https://api.shngm.io/v1/manga/detail/:id",
-      // },
-      // {
-      //   source: "/v1/api/mangas/:params*",
-      //   destination: "https://api.shngm.io/v1/manga/list/:params*",
-      // },
-    ];
-  },
 };
 
 export default nextConfig;
