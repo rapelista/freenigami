ea27c6c84c6a2e04125f7c08b031c4a9f71c3f9e
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-10-02 00:46:32 +0700

    feat: initialize Next.js project with Tailwind CSS and basic layout
    - Add PostCSS configuration for Tailwind CSS
    - Create RootLayout component for application structure
    - Implement Home page with a simple greeting
    - Define site metadata for SEO
    - Add global CSS file to import Tailwind styles
    - Set up TypeScript configuration for the project

---
+++ eslint.config.mjs
@@ -0,0 +1,118 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+
+  {
+    plugins: {
+      '@tanstack/query': pluginQuery,
+    },
+    rules: {
+      '@tanstack/query/exhaustive-deps': 'error',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      '@typescript-eslint/triple-slash-reference': 'off',
+    },
+  },
+  {
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'type',
+            'builtin',
+            'object',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              group: 'external',
+              pattern: '~/**',
+              position: 'after',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      'no-console': 'warn',
+
+      'padding-line-between-statements': [
+        'warn',
+        {
+          blankLine: 'always',
+          next: 'return',
+          prev: '*',
+        },
+        {
+          blankLine: 'always',
+          next: '*',
+          prev: ['const', 'let', 'var'],
+        },
+        {
+          blankLine: 'any',
+          next: ['const', 'let', 'var'],
+          prev: ['const', 'let', 'var'],
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      'react/jsx-curly-brace-presence': [
+        'warn',
+        {
+          children: 'never',
+          props: 'never',
+        },
+      ],
+
+      'react/jsx-sort-props': [
+        'warn',
+        {
+          callbacksLast: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+          shorthandFirst: true,
+        },
+      ],
+
+      'react/react-in-jsx-scope': 'off',
+
+      'react/self-closing-comp': 'warn',
+    },
+  },
+];
+
+export default eslintConfig;
