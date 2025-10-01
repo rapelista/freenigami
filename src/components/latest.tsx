a018a7f32f3302bd24804ef72ad30479ad21c934
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-10-02 02:04:54 +0700

    feat: enhance Latest component with data fetching and display using TRPC and React Query

--- src/components/latest.tsx
+++ src/components/latest.tsx
@@ -1,8 +1,14 @@
 'use client';
 
-import { Skeleton, Tabs } from '@heroui/react';
+import { Button, Skeleton, Tabs } from '@heroui/react';
+import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
+
+import { trpc } from '~/trpc/client';
 
 export function Latest() {
+  const { data } = useQuery(trpc.series.list.queryOptions());
+
   return (
     <div className="space-y-4">
       <Tabs className="w-full max-w-md">
@@ -16,9 +22,38 @@ export function Latest() {
       </Tabs>
 
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4 xl:grid-cols-6">
-        {Array.from({ length: 24 }, (_, i) => (
-          <div key={i}>
-            <Skeleton className="w-full aspect-[5/9]" />
+        {data?.data.map((series) => (
+          <div
+            key={series.manga_id}
+            className="aspect-[5/11] flex flex-col gap-2 justify-end"
+          >
+            <Skeleton className="flex-1" />
+
+            <div className="flex items-center justify-center p-2 min-h-16">
+              <Link
+                className="line-clamp-2 text-center"
+                href={`/series/${series.manga_id}`}
+              >
+                {series.title}
+              </Link>
+            </div>
+
+            <div className="grid gap-2">
+              {series.chapters.slice(0, 2).map((chapter) => (
+                <Button
+                  key={chapter.chapter_id}
+                  asChild
+                  size="sm"
+                  variant="secondary"
+                >
+                  <Link
+                    href={`/series/${series.manga_id}/chapters/${chapter.chapter_id}`}
+                  >
+                    Chapter {chapter.chapter_number}
+                  </Link>
+                </Button>
+              ))}
+            </div>
           </div>
         ))}
       </div>
--- src/trpc/router/index.ts
+++ src/trpc/router/index.ts
@@ -1,15 +1,52 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
 import { appProcedure, router } from '~/trpc/init';
+
+const schema = z.object({
+  data: z.array(
+    z.object({
+      manga_id: z.string(),
+      title: z.string(),
+      alternative_title: z.string(),
+      country_id: z.string(),
+      cover_image_url: z.string(),
+      cover_portrait_url: z.string(),
+      chapters: z.array(
+        z.object({
+          chapter_id: z.string(),
+          chapter_number: z.number(),
+          created_at: z.string(),
+        }),
+      ),
+    }),
+  ),
+});
 
 export const appRouter = router({
   series: router({
-    list: appProcedure.query(() => {
-      return {
-        data: [
-          {
-            id: 1,
-          },
-        ],
-      };
+    list: appProcedure.query(async () => {
+      const url = new URL(
+        'https://api.shngm.io/v1/manga/list?type=project&page=1&page_size=24&is_update=true&sort=latest&sort_order=desc',
+      );
+
+      const response = await fetch(url.toString());
+
+      if (!response.ok) {
+        throw new TRPCError({
+          code: 'BAD_GATEWAY',
+        });
+      }
+
+      const data = await response.json();
+
+      try {
+        return schema.parse(data);
+      } catch {
+        throw new TRPCError({
+          code: 'BAD_GATEWAY',
+        });
+      }
     }),
   }),
 });
