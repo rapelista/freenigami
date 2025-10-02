6bb5cd52ed00ce74b9692a929c924911658d9111
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-10-02 23:13:57 +0700

    feat: add PaginationSchema for consistent pagination handling in API queries

--- src/trpc/router/index.ts
+++ src/trpc/router/index.ts
@@ -2,87 +2,138 @@ import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 import { generateListSchema } from '~/schema/list';
+import { PaginationSchema } from '~/schema/pagination';
 import { SeriesSchema } from '~/schema/series';
 import { appProcedure, router } from '~/trpc/init';
 
 export const appRouter = router({
   series: router({
-    list: appProcedure.query(async () => {
+    list: appProcedure
+      .input(
+        PaginationSchema.extend({
+          q: z.string(),
+        })
+          .partial()
+          .optional()
+          .default({
+            page: 1,
+            page_size: 24,
+          }),
+      )
+      .query(async ({ input }) => {
+        try {
+          const url = new URL('https://api.shngm.io/v1/manga/list');
+          const response = await fetch(url.toString());
+
+          Object.entries(input).forEach(([key, value]) => {
+            if (value) {
+              url.searchParams.set(key, String(value));
+            }
+          });
+
+          if (!response.ok) {
+            throw new Error();
+          }
+
+          const data = await response.json();
+
+          return generateListSchema(SeriesSchema).parse(data);
+        } catch {
+          throw new TRPCError({
+            code: 'BAD_GATEWAY',
+          });
+        }
+      }),
+
+    recommendation: appProcedure.query(async () => {
+      const url = new URL('https://api.shngm.io/v1/manga/list');
+
+      url.searchParams.set('format', 'manhwa');
+      url.searchParams.set('page', '1');
+      url.searchParams.set('page_size', '8');
+      url.searchParams.set('is_recommended', 'true');
+      url.searchParams.set('sort', 'latest');
+      url.searchParams.set('sort_order', 'desc');
+
+      const response = await fetch(url.toString());
+
+      if (!response.ok) {
+        throw new TRPCError({
+          code: 'BAD_GATEWAY',
+        });
+      }
+
+      const data = await response.json();
+
       try {
+        return generateListSchema(SeriesSchema).parse(data);
+      } catch {
+        throw new TRPCError({
+          code: 'BAD_GATEWAY',
+        });
+      }
+    }),
+
+    latest: appProcedure
+      .input(
+        PaginationSchema.extend({
+          type: z.string(),
+        })
+          .partial()
+          .optional()
+          .default({
+            page: 1,
+            page_size: 24,
+            type: 'project',
+          }),
+      )
+      .query(async ({ input }) => {
         const url = new URL('https://api.shngm.io/v1/manga/list');
+
+        /**
+         * Default Search Parameters
+         */
+        url.searchParams.set('is_update', 'true');
+        url.searchParams.set('sort', 'latest');
+        url.searchParams.set('sort_order', 'desc');
+
+        /**
+         * Dynamic Search Parameters
+         */
+        Object.entries(input).forEach(([key, value]) => {
+          if (value) {
+            url.searchParams.set(key, String(value));
+          }
+        });
+
         const response = await fetch(url.toString());
 
         if (!response.ok) {
-          throw new Error();
+          throw new TRPCError({
+            code: 'BAD_GATEWAY',
+          });
         }
 
         const data = await response.json();
 
-        return generateListSchema(SeriesSchema).parse(data);
-      } catch {
-        throw new TRPCError({
-          code: 'BAD_GATEWAY',
-        });
-      }
-    }),
-
-    recommendation: appProcedure.query(async () => {
-      const url = new URL(
-        'https://api.shngm.io/v1/manga/list?format=manhwa&page=1&page_size=8&is_recommended=true&sort=latest&sort_order=desc',
-      );
-
-      const response = await fetch(url.toString());
-
-      if (!response.ok) {
-        throw new TRPCError({
-          code: 'BAD_GATEWAY',
-        });
-      }
-
-      const data = await response.json();
-
-      try {
-        return generateListSchema(SeriesSchema).parse(data);
-      } catch {
-        throw new TRPCError({
-          code: 'BAD_GATEWAY',
-        });
-      }
-    }),
-
-    latest: appProcedure.query(async () => {
-      const url = new URL(
-        'https://api.shngm.io/v1/manga/list?type=project&page=1&page_size=24&is_update=true&sort=latest&sort_order=desc',
-      );
-
-      const response = await fetch(url.toString());
-
-      if (!response.ok) {
-        throw new TRPCError({
-          code: 'BAD_GATEWAY',
-        });
-      }
-
-      const data = await response.json();
-
-      try {
-        return generateListSchema(
-          SeriesSchema.omit({ taxonomy: true }).extend({
-            chapters: z.array(
-              z.object({
-                chapter_id: z.string(),
-                chapter_number: z.number(),
-                created_at: z.string(),
-              }),
-            ),
-          }),
-        ).parse(data);
-      } catch {
-        throw new TRPCError({
-          code: 'BAD_GATEWAY',
-        });
-      }
-    }),
+        try {
+          return generateListSchema(
+            SeriesSchema.omit({ taxonomy: true }).extend({
+              chapters: z.array(
+                z.object({
+                  chapter_id: z.string(),
+                  chapter_number: z.number(),
+                  created_at: z.string(),
+                }),
+              ),
+            }),
+          ).parse(data);
+        } catch {
+          throw new TRPCError({
+            code: 'BAD_GATEWAY',
+          });
+        }
+      }),
   }),
 });
 
