ace46b60a60eefdac71481165ab221fc462a9bd6
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-10-04 14:06:45 +0700

    feat: add transition effects to top and bottom sections in ChapterDetail component

--- src/components/chapter/[id]/index.tsx
+++ src/components/chapter/[id]/index.tsx
@@ -84,7 +84,7 @@ export function ChapterDetail() {
     <>
       <div
         ref={topRef}
-        className="sticky opacity-100 top-0 border-y bg-surface-2 z-10"
+        className="sticky transition-opacity duration-250 ease-smooth opacity-100 top-0 border-y bg-surface-2 z-10"
       >
         <div className="container mx-auto px-4 py-2.5 md:px-0 md:py-5 flex justify-between gap-6">
           <h2
@@ -117,7 +117,7 @@ export function ChapterDetail() {
 
       <div
         ref={bottomRef}
-        className="sticky opacity-100 bottom-0 border-y bg-surface-2 z-10"
+        className="sticky transition-opacity duration-250 ease-smooth opacity-100 bottom-0 border-y bg-surface-2 z-10"
       >
         <div className="container mx-auto px-4 py-2.5 md:px-0 md:py-5 flex justify-between gap-6">
           <Link
