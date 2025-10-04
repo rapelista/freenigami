c546a8355c97adbe6ebd3c78e4fb827f11af9032
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-10-04 09:14:26 +0700

    feat: update layout structure and styling for header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,24 @@ export default function RootLayout({
       <body>
         <Providers>
           <header className="border-b-[0.5px] border-dashed">
-            <div className="container mx-auto p-4 md:p-6 grid md:grid-cols-3">
+            <div className="container mx-auto p-4 md:p-6 grid grid-cols-2 md:grid-cols-3">
               <Link
-                className="capitalize font-medium md:text-xl md:col-start-2 md:place-self-center"
+                className="capitalize font-medium md:text-xl md:col-start-2 place-self-start md:place-self-center"
                 href="/"
               >
                 FREENIGAMI
               </Link>
+
+              <ul className="place-self-end">
+                <li>
+                  <Link
+                    className="hover:underline underline-offset-4"
+                    href="/explore"
+                  >
+                    Explore
+                  </Link>
+                </li>
+              </ul>
             </div>
           </header>
 
