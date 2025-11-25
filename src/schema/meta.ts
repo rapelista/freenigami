4740841f95a6d4beeddcd374d01e27a82ffc675f
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-11-25 16:38:28 +0700

    refactor: set default values for MetaSchema properties

--- src/schema/meta.ts
+++ src/schema/meta.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const MetaSchema = z.object({
-  page: z.number(),
-  page_size: z.number(),
-  total_page: z.number(),
-  total_record: z.number(),
+  page: z.number().default(1),
+  page_size: z.number().default(1),
+  total_page: z.number().default(1),
+  total_record: z.number().default(0),
 });
 
 export type Meta = z.infer<typeof MetaSchema>;
