b6a3b42c2c1293c3f2a9abb1ebacf04f70cf4c12
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-11-28 10:38:50 +0700

    refactor: update image source in Playground page to use proxy API

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image';
 
 export default function Page() {
-  const src =
-    'https://storage.shngm.id/low/unsafe/filters:format(webp):quality(70)/thumbnail/image/3b04e4c6-0ea3-4afb-a1bd-b85e54c912a4.jpg';
+  const src = '/api/proxy/image/3b04e4c6-0ea3-4afb-a1bd-b85e54c912a4.jpg';
 
   return <Image alt="Wow" height={1440} src={src} width={720} />;
 }
