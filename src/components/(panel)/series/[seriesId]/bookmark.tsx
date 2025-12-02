0903b4167a68145f757cbde88a1ef9a82244ea89
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-12-02 12:13:35 +0700

    refactor(bookmark): clean up unused code and improve bookmark handling logic

--- src/components/(panel)/series/[seriesId]/bookmark.tsx
+++ src/components/(panel)/series/[seriesId]/bookmark.tsx
@@ -3,37 +3,29 @@
 import { Checkbox, Description, Label } from '@heroui/react';
 import { BookmarkIcon } from 'lucide-react';
 
-import { cn } from '~/lib/utils';
-// import { BookmarkType } from '~/lib/enum';
-// import { checkIsBookmarked, cn } from '~/lib/utils';
-// import { useBookmarkStore, type Bookmark } from '~/stores/bookmark';
+import { BookmarkType } from '~/lib/enum';
+import { checkIsBookmarked, cn } from '~/lib/utils';
+import { useBookmarkStore, type Bookmark } from '~/stores/bookmark';
 
 interface SeriesBookmarkProps {
   seriesId: string;
 }
 
-const addon = {
-  description: 'Browser and mobile push alerts',
-  icon: 'gravity-ui:bell',
-  title: 'Push Notifications',
-  value: 'push',
-};
+export function SeriesBookmark({ seriesId }: SeriesBookmarkProps) {
+  const bookmarks = useBookmarkStore((state) => state.bookmarks);
+  const addBookmark = useBookmarkStore((state) => state.addBookmark);
+  const removeBookmark = useBookmarkStore((state) => state.removeBookmark);
 
-export function SeriesBookmark({ seriesId: _ }: SeriesBookmarkProps) {
-  // const bookmarks = useBookmarkStore((state) => state.bookmarks);
-  // const addBookmark = useBookmarkStore((state) => state.addBookmark);
-  // const removeBookmark = useBookmarkStore((state) => state.removeBookmark);
+  const bookmark: Bookmark = { type: BookmarkType.SERIES, value: seriesId };
+  const isBookmarked = checkIsBookmarked(bookmarks, bookmark);
 
-  // const bookmark: Bookmark = { type: BookmarkType.SERIES, value: seriesId };
-  // const isBookmarked = checkIsBookmarked(bookmarks, bookmark);
-
-  // const handleToggleBookmark = () => {
-  //   if (isBookmarked) {
-  //     removeBookmark(bookmark);
-  //   } else {
-  //     addBookmark(bookmark);
-  //   }
-  // };
+  const handleToggleBookmark = () => {
+    if (isBookmarked) {
+      removeBookmark(bookmark);
+    } else {
+      addBookmark(bookmark);
+    }
+  };
 
   return (
     <>
@@ -43,7 +35,8 @@ export function SeriesBookmark({ seriesId: _ }: SeriesBookmarkProps) {
           'bg-surface group relative flex-col gap-4 rounded-3xl px-5 py-4 transition-all',
           'data-[selected=true]:bg-accent/10',
         )}
-        value={addon.value}
+        isSelected={isBookmarked}
+        onChange={handleToggleBookmark}
       >
         <Checkbox.Control className="absolute right-4 top-3 size-5 rounded-full before:rounded-full">
           <Checkbox.Indicator />
